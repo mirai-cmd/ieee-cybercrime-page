5f3ec7c7b523b824c4ea47ca8e38fe09b052af51
ElliotAlderson-cmd <prajwalghotage2508@gmail.com>
2023-12-12 02:22:38 +0530

    Added geolocation and adutomatic  file download

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
-function getIP(){
-  fetch("https://api.ipify.org",{
-    method: "GET"
-  }).then((res)=>{
-    console.log("IP is ",res.ip)
-  }).catch((err)=>{
-    console.log("Error",err)
-  })
-  return;
-}
-function getfile() {
- var textFile=null;
-  var text =
-    "Merry Christmas!!\nThis is a test.\nIf you are seeing this then its for the best, this is how dangerous files are used to inflict damage.\nThank you for being a part of this experiment.\nStay safe and have a nice day :)";
+async function downloadBadFile() {
+  var textFile = null;
+  const res = await fetch("https://ipinfo.io/json?token=e3649d97b03768");
+  const geo_data = await res.json();
+  console.log(geo_data);
+  var text = `Merry Christmas!!\n\n${
+    geo_data
+      ? `Your public IP address: ${geo_data.ip}\nCity : ${geo_data.city}\nRegion : ${geo_data.region}\n`
+      : ""
+  }\nThis is a test.\nIf you are seeing this then its for the best, this is how dangerous files are used to inflict damage.\nThank you for being a part of this experiment.\nStay safe and have a nice day :)`;
   var data = new Blob([text], { type: "text/plain" });
   if (textFile !== null) {
     window.URL.revokeObjectURL(textFile);
   }
   textFile = window.URL.createObjectURL(data);
-  return textFile;
-}
-function downloadBadFile() {
   var link = document.createElement("a");
   var name = "badfile.txt";
   link.download = name;
-  link.href = getfile();
+  link.href = textFile;
+  link.target = "_blank";
   link.click();
   delete link;
 }
